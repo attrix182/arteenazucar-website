5e9854bbc825379bf260007393e755e07c523587
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-05-30 22:29:01 -0300

    Creo componente topbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
