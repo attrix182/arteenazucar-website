b538d3d4fff8773ae813fab03017697fc106b086
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-05-30 23:27:29 -0300

    Creo componente about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
+import { AboutUsComponent } from './sections/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    TopbarComponent
+    TopbarComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
